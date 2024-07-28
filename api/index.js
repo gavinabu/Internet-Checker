dd5d1c994485eade0b91b4237c269ad9566f5c4a
Gavin Abu-Zahra <gavinabuzahra@justwhatever.net>
2024-07-28 01:18:30 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,10 +7,13 @@ app.use(cors({origin:"*"}))
 app.get("/",(req,res) => {
   console.log(req)
   res.json({
-    ip:req.ip,
-    ips:req.ips,
+    ip:req.headers["x-real-ip"],
+    rawIp:req.ip,
+    serverTime:Date.now(),
+    timeZone:req.headers["x-vercel-ip-timezone"],
+    useragents:req.headers["user-agent"],
+    platform:req.headers["Sec-Ch-Ua-Platform"],
     headers:req.headers,
-    rawheaders:req.rawheaders,
   })
 })
 
@@ -19,6 +22,10 @@ app.get("/getTime",(req,res) => {
   res.send(Date.now())
 })
 
+app.use((req,res) => {
+  res.setHeader("Content-Type","text/plain")
+  res.send("Error 404")
+})
 
 app.listen(80, () => {
   console.log("App running")
